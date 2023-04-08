0c70774c071c2a930c247a72dc0f8b862edf2f7a
Robby Evans <ruttoevans001@gmail.com>
2023-04-08 21:20:31 +0300

    feature: shopping cart

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,23 +1,18 @@
-import React, { FC } from "react";
-import storeItems from "../data/items.json";
 import { Col, Row } from "react-bootstrap";
-import StoreItem from "../components/StoreItem";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
 
-interface StoreProps {}
-
-const Store: FC<StoreProps> = ({}) => {
+export function Store() {
   return (
     <>
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
-        {storeItems.map((items) => (
-          <Col key={items.id}>
-            <StoreItem {...items} />
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
           </Col>
         ))}
       </Row>
     </>
   );
-};
-
-export default Store;
+}
